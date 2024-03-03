ab16a60465677618a7d0a28eeff230b3498111ce
Cleberton Kobay <Clebertonkobay@gmail.com>
2024-03-02 23:19:36 -0300

    Configurações iniciais do CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
+import { StartScreen } from './components/StartScreen'
+export function App() {
 
   return (
-    <>
-      <div>
-        
-      </div>
-    </>
+      <h1 className='App'>
+        <StartScreen />
+      </h1>
   )
 }
-
-export default App
